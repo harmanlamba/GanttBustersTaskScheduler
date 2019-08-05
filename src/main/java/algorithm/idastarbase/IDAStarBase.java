06dd4eeda5fcabfa7c7b6568439219323abf05dc
Gargi Bansal <gban550@aucklanduni.ac.nz>
2019-08-06 11:40:09 +1200

    Partially calculated a potential lower bound. Gargi, Jainal.

--- src/main/java/algorithm/idastarbase/IDAStarBase.java
+++ src/main/java/algorithm/idastarbase/IDAStarBase.java
@@ -6,6 +6,7 @@ import graph.GraphNode;
 import graph.OutputGraphNode;
 
 import java.util.Map;
+import java.util.Set;
 
 public class IDAStarBase extends Algorithm {
 
@@ -17,5 +18,14 @@ public class IDAStarBase extends Algorithm {
     public Map<GraphNode, OutputGraphNode> solve() {
         getTopologicalOrdering();
         return null;
+    }
+
+    private int calcWeightProcRatio() {
+        Set<GraphNode> allNodes = _graph.getGraph().vertexSet();
+        int total = 0;
+        for (GraphNode node: allNodes) {
+            total += node.getWeight();
+        }
+        return total/_numProcTask;
     }
 }
