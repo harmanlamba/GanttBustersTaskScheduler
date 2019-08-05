2f01cd351edb112cb86f140445aac7c139e8499b
JainalGandhi <jainalgandhi@gmail.com>
2019-08-06 10:28:47 +1200

    Implemented Algorithm Builder[Harman, Jainal, Preet]

--- src/main/java/algorithm/AlgorithmBuilder.java
+++ src/main/java/algorithm/AlgorithmBuilder.java
@@ -1,4 +1,21 @@
 package algorithm;
 
+import algorithm.common.utility.AlgorithmType;
+import algorithm.idastarbase.IDAStarBase;
+import algorithm.idastarparallel.IDAStarParallel;
+import algorithm.sequential.Sequential;
+import graph.Graph;
+
 public class AlgorithmBuilder {
+
+    public Algorithm AlgorithmBuilder(AlgorithmType type, Graph graph) {
+        switch (type) {
+            case Idstarbase:
+                return new IDAStarBase(graph);
+            case Idstarparallel:
+                return new IDAStarParallel(graph);
+            default:
+                return new Sequential(graph);
+        }
+    }
 }
