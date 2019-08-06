a0132f59508b78811f26f3b880959af1991b8cd8
Preet Patel <iam@preetpatel.com>
2019-08-06 13:44:50 +1200

    added command line input params

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -6,8 +6,6 @@ import algorithm.common.utility.AlgorithmType;
 import fileio.IO;
 import graph.Graph;
 
-import java.nio.file.Paths;
-
 /**
  * Hello world!
  *
@@ -16,14 +14,93 @@ public class App
 {
     public static void main( String[] args )
     {
-        for(int i=0;i< args.length;i++) {
-            System.out.println(args[i]);
+        // Checks for minimum number of required parameters
+        if (args.length < 2) {
+            printUsage();
+            System.exit(401);
         }
-        IO io = new IO("example.dot", "hello.dot");
+
+        int numberOfProcessorsForParallelAlgorithm = 0;
+        int numberOfProcessorsForTask = 1;
+        String nameOfInputFile = args[0];
+        String nameOfOutputFile = "";
+
+        // Created an automated out
+        if (args[0].contains(".")) {
+            String[] fileNameSplit = args[0].split("\\.");
+            nameOfOutputFile = fileNameSplit[0] + "-output.dot";
+        } else {
+            System.out.println("Invalid file format. Accepted: .dot files");
+            printUsage();
+            System.exit(401);
+        }
+
+
+        try {
+            numberOfProcessorsForTask = Integer.parseInt(args[1]);
+        } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+            System.err.println("Invalid number of processors to allocate tasks on");
+            printUsage();
+            System.exit(401);
+        }
+
+        for (int i = 2; i < args.length; i++) {
+            if (args[i].equals("-p")) {
+                try {
+                    numberOfProcessorsForParallelAlgorithm = Integer.parseInt(args[i + 1]);
+                } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+                    System.err.println("Invalid number of processors for parallelism");
+                    printUsage();
+                    System.exit(401);
+                }
+                i = i +1;
+            }
+
+             else if (args[i].equals("-v")) {
+                //TODO Add visualisation stuff
+            }
+
+             else if (args[i].equals("-o")) {
+                try {
+                    nameOfOutputFile = args[i + 1].contains(".") ? args[i + 1] : args[i + 1] + ".dot";
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    System.err.println("Invalid output file name");
+                    printUsage();
+                    System.exit(401);
+                }
+                i = i + 1;
+            }
+
+             else {
+                 System.err.println("Invalid input arguments");
+                 printUsage();
+                 System.exit(401);
+            }
+        }
+
+        System.out.println("Input File name: " + nameOfInputFile);
+        System.out.println("Output File name: " + nameOfOutputFile);
+        System.out.println("pTask: " + numberOfProcessorsForTask);
+        System.out.println("pParallelTask: " + numberOfProcessorsForParallelAlgorithm);
+
+
+        IO io = new IO(nameOfInputFile, nameOfOutputFile);
         Graph graph = new Graph(io.getNodeMap(), io.getEdgeList());
-        AlgorithmBuilder algorithmBuilder = new AlgorithmBuilder(AlgorithmType.SEQUENTIAL, graph, 1, 0);
+        AlgorithmBuilder algorithmBuilder = new AlgorithmBuilder(AlgorithmType.SEQUENTIAL, graph, numberOfProcessorsForTask, numberOfProcessorsForParallelAlgorithm);
         Algorithm algorithm = algorithmBuilder.getAlgorithm();
         algorithm.solve();
+    }
+
+    private static void printUsage() {
+        System.err.println("Usage: ");
+        System.err.println("java -jar scheduler.jar INPUT.dot P [OPTION]");
+        System.err.println("INPUT.dot    a task graph with integer weights in dot format");
+        System.err.println("P            number of processors to schedule the INPUT graph on\n\n");
+        System.err.println("Optional:");
+        System.err.println("-p N        use N cores for execution in parallel (default is sequential)");
+        System.err.println("-v          visualise the search");
+        System.err.println("-o OUTPUT   output file is named OUTPUT (default is INPUT-output.dot)");
+
     }
 
 // DON'T REMOVE THIS
