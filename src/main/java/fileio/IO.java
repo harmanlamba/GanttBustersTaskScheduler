d61c3c4712dbfedf07cf5fb122c7496c510e49be
itsercleung <leung.eric24@gmail.com>
2019-08-08 18:24:27 +1200

    Added exceptions and commenting IO lines [Harman, Eric, Salma, Jainal]

--- src/main/java/fileio/IO.java
+++ src/main/java/fileio/IO.java
@@ -3,13 +3,12 @@ package fileio;
 import algorithm.common.utility.Utility;
 import graph.GraphEdge;
 import graph.GraphNode;
-
 import java.io.File;
 import java.util.List;
 import java.util.Map;
-
 import static algorithm.common.utility.Utility.printUsage;
 
+//TODO: class comment
 public class IO implements IIO {
 
     private Read _read;
@@ -21,6 +20,10 @@ public class IO implements IIO {
         validateAndParseInput(input);
     }
 
+    /**
+     * validateAndParseInput - Get console inputs and validate if valid user input
+     * @param args - user input from console
+     */
     private void validateAndParseInput(String[] args) {
         // Checks for minimum number of required parameters
         if (args.length < 2) {
@@ -28,13 +31,14 @@ public class IO implements IIO {
             System.exit(401);
         }
 
+        //Instantiate initial fields for parsing
         _numberOfProcessorsForParallelAlgorithm = 0;
         _numberOfProcessorsForTask = 1;
         String nameOfInputFile = args[0];
         String nameOfOutputFile = "";
         String parentPath = "";
 
-        // Created an automated out
+        // Created an automated out - check for exists and parse appropriate filename
         File file = new File(args[0]);
         if (file.exists()) {
             File parentFolder = file.getParentFile();
@@ -49,7 +53,7 @@ public class IO implements IIO {
             System.exit(401);
         }
 
-
+        //Set user input with number of processors
         try {
             _numberOfProcessorsForTask = Integer.parseInt(args[1]);
         } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
@@ -58,62 +62,85 @@ public class IO implements IIO {
             System.exit(401);
         }
 
+        //Assign number of processors to algorithm parallel
         for (int i = 2; i < args.length; i++) {
-            if (args[i].equals("-p")) {
-                try {
-                    _numberOfProcessorsForParallelAlgorithm = Integer.parseInt(args[i + 1]);
-                } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
-                    System.err.println("Invalid number of processors for parallelism");
+            switch (args[i]) {
+                //Use multiple processors
+                case "-p":
+                    try {
+                        _numberOfProcessorsForParallelAlgorithm = Integer.parseInt(args[i + 1]);
+                    } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+                        System.err.println("Invalid number of processors for parallelism");
+                        printUsage();
+                        System.exit(401);
+                    }
+                    i++;
+                    break;
+                //Use visualization GUI with given file
+                case "-v":
+                    //TODO Add visualisation stuff
+                    break;
+                //Set output file name (if needed)
+                case "-o":
+                    try {
+                        nameOfOutputFile = args[i + 1].contains(".") ? args[i + 1] : args[i + 1] + ".dot";
+                        nameOfOutputFile = parentPath + "/" + nameOfOutputFile;
+                    } catch (ArrayIndexOutOfBoundsException e) {
+                        System.err.println("Invalid output file name");
+                        printUsage();
+                        System.exit(401);
+                    }
+                    i++;
+                    break;
+                default:
+                    System.err.println("Invalid input arguments");
                     printUsage();
                     System.exit(401);
-                }
-                i++;
-            }
-
-            else if (args[i].equals("-v")) {
-                //TODO Add visualisation stuff
-            }
-
-            else if (args[i].equals("-o")) {
-                try {
-                    nameOfOutputFile = args[i + 1].contains(".") ? args[i + 1] : args[i + 1] + ".dot";
-                    nameOfOutputFile = parentPath + "/" + nameOfOutputFile;
-                } catch (ArrayIndexOutOfBoundsException e) {
-                    System.err.println("Invalid output file name");
-                    printUsage();
-                    System.exit(401);
-                }
-                i++;
-            }
-
-            else {
-                System.err.println("Invalid input arguments");
-                printUsage();
-                System.exit(401);
             }
         }
 
+        //Apply guardnull on read and write input - checking for exist of nulls
         _read = (Read) Utility.GuardNull(new Read(nameOfInputFile));
         _read.readFile();
         _write = (Write) Utility.GuardNull(new Write(nameOfInputFile, nameOfOutputFile));
     }
 
+    /**
+     * getNodeMap - returns node map for graph nodes
+     * @return map of nodes to assign to JGraphT instantiation
+     */
     public Map<String, GraphNode> getNodeMap() {
         return _read.getNodeMap();
     }
 
+    /**
+     * getEdgeList - returns edge list for graph edges
+     * @return list of edges to assign to JGraphT instantiation
+     */
     public List<GraphEdge> getEdgeList() {
         return _read.getEdgeList();
     }
 
+    /**
+     * write - write resulting map of nodes to output file
+     * @param algorithmResultMap - result map from algorithm result
+     */
     public void write(Map<String, GraphNode> algorithmResultMap) {
         _write.writeToPath(algorithmResultMap);
     }
 
+    /**
+     * getNumberOfProcessorsForParallelAlgorithm - returns number of assigned processors of Parallel Algorithm
+     * @return number of processors used
+     */
     public int getNumberOfProcessorsForParallelAlgorithm() {
         return _numberOfProcessorsForParallelAlgorithm;
     }
 
+    /**
+     * getNumberOfProcessorsForTask - returns number of assigned tasks of Parallel Algorithm
+     * @return number of tasks used
+     */
     public int getNumberOfProcessorsForTask() {
         return _numberOfProcessorsForTask;
     }
