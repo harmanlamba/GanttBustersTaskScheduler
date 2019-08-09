18be53477d0176080fbf4b10622c63480e8d3384
Gargi Bansal <gban550@aucklanduni.ac.nz>
2019-08-10 11:41:01 +1200

    Added comments for sequential Gargi

--- src/main/java/algorithm/sequential/Sequential.java
+++ src/main/java/algorithm/sequential/Sequential.java
@@ -7,17 +7,34 @@ import graph.GraphNode;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * A child class of algorithm which sequentially solves the task scheduling problem on one processor
+ */
 public class Sequential extends Algorithm {
 
+    /**
+     * Instantiates a Sequential instance
+     * @param g is a graph of the network
+     * @param numProcTask is the number of processors that the tasks needed to be scheduled onto
+     * @param numProcParallel is the number of processors the algorithm should be working on
+     */
     public Sequential(Graph g, int numProcTask, int numProcParallel) {
         super(g, numProcTask, numProcParallel);
     }
 
+    /**
+     * Method that solves the problem sequentially on one processor
+     * @return A map of the nodes with their corresponding start times (string is the name of the
+     * node and GraphNode contains all of the node information)
+     */
     @Override
     public Map<String, GraphNode> solve() {
+
+        // Gets topological order of the network graph and puts order into field
         getTopologicalOrdering();
         Map<String, GraphNode> output = new HashMap<>();
 
+        // Creates output format of ordering and scheduling (start times for sequential)
         int currentTime = 0;
         for (int i = 0; i < _order.size(); i++) {
             GraphNode tempNode = _order.get(i);
