d4b9c9b066a54ea3ea5a180283edb3f5a7f9cadd
itsercleung <leung.eric24@gmail.com>
2019-08-10 14:48:19 +1200

    Commenting utility, exceptions, unit test, graph [Harman, Eric]

--- src/main/java/graph/Graph.java
+++ src/main/java/graph/Graph.java
@@ -8,23 +8,31 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-//TODO: Class comments + Method comments
+/**
+ * Graph - instantiates and holds JGraphT using IO's graph node map and graph edge lists.
+ */
 public class Graph {
 
-    //TODO: Clean this field?
-    private DirectedWeightedMultigraph<GraphNode, DefaultWeightedEdge> _jGraph = (DirectedWeightedMultigraph<GraphNode, DefaultWeightedEdge>) Utility.GuardNull(new DirectedWeightedMultigraph<>(DefaultWeightedEdge.class));
+    private DirectedWeightedMultigraph<GraphNode, DefaultWeightedEdge> _jGraph = (DirectedWeightedMultigraph<GraphNode, DefaultWeightedEdge>)
+            Utility.GuardNull(new DirectedWeightedMultigraph<>(DefaultWeightedEdge.class)); //Hold retrievable jGraphT
 
     public Graph(Map<String, GraphNode> vertexMap, List<GraphEdge> edgeList) {
+        //Assign graph node map to JGraphT
         for (GraphNode graphNode : new ArrayList<>(vertexMap.values())) {
             _jGraph.addVertex(graphNode);
         }
 
+        //Assign graph edge list to JGraphT
         for (GraphEdge graphEdge : edgeList) {
             DefaultWeightedEdge edge = _jGraph.addEdge(graphEdge.getEdgeFrom(), graphEdge.getEdgeTo());
             _jGraph.setEdgeWeight(edge, graphEdge.getEdgeWeight());
         }
     }
 
+    /**
+     * getGraph - getter for JGraphT DAG for algorithm's use
+     * @return DirectedWeightedMultigraph from JGraphT
+     */
     public DirectedWeightedMultigraph getGraph() {
         return _jGraph;
     }
