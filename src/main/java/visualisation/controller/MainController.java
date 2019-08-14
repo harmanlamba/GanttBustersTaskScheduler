334b858f27ccbc09709b0c44208aec59c09eca4d
itsercleung <leung.eric24@gmail.com>
2019-08-14 18:33:25 +1200

    Added more fxml [Harman, Eric]

--- src/main/java/visualisation/controller/MainController.java
+++ src/main/java/visualisation/controller/MainController.java
@@ -1,24 +1,39 @@
 package visualisation.controller;
 
-import javafx.fxml.FXML;
+import com.jfoenix.controls.JFXTreeTableView;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.control.TreeTableColumn;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
+import javax.xml.soap.Text;
+
 public class MainController implements IObserver {
 
-    @FXML private HBox mainContainer;
-    @FXML private VBox statsContainer;
-    @FXML private TabPane visualsContainer;
-    @FXML private Tab graphTab;
-    @FXML private Tab taskTab;
-    @FXML private Tab resultTab;
+    private HBox mainContainer;
+    private VBox statsContainer;
+    private Text algorithmTypeText;
+    private Text timeElapsedText;
+    private Text numProcessorsText;
+    private Text parallelProcessorsText;
+    private Text memoryUsedText;
+    private TabPane visualsContainer;
+    private Tab graphTab;
+    private Tab taskTab;
+    private Tab resultTab;
+    private JFXTreeTableView<?> scheduleResultsTable;
+    private TreeTableColumn<?, ?> taskIDColumn;
+    private TreeTableColumn<?, ?> startTimeColumn;
+    private TreeTableColumn<?, ?> assignedProcessorColumn;
 
     //Reference to the ALgorithm, in order to know when we get notified about the specific stats such as Branch
     //Pruning
     private IObservable _observableAlgorithm;
 
+    public MainController() {
+
+    }
     public MainController(IObservable observableAlgorithm){
         _observableAlgorithm=observableAlgorithm;
     }
