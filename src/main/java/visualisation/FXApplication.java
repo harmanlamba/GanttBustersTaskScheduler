249dade82a6a48a65ae2e9a3c13ac96f43e290b8
Harman Lamba <harrygth15478@gmail.com>
2019-08-20 22:35:50 +1200

    Changed to a lambda expression thread call [Harman]

--- src/main/java/visualisation/FXApplication.java
+++ src/main/java/visualisation/FXApplication.java
@@ -50,11 +50,7 @@ public class FXApplication extends Application {
         Algorithm algorithm = AlgorithmBuilder.getAlgorithmBuilder().getAlgorithm();
         IIO io = App._mainIO;
         //Runs the algorithm in a new thread
-        new Thread() {
-            public void run() {
-                io.write(AlgorithmBuilder.getAlgorithmBuilder().getAlgorithm().solveAlgorithm());
-            }
-        }.start();
+        new Thread(() -> io.write(AlgorithmBuilder.getAlgorithmBuilder().getAlgorithm().solveAlgorithm())).start();
         System.out.println("Finish");
 
 
