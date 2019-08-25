6cce146360871f0d8715a7c9544545a0f7a0c065
salma-s <ssan631@aucklanduni.ac.nz>
2019-08-26 01:46:55 +1200

    Doco done-o

--- src/main/java/algorithm/BBAStarParallel/IBBAObservable.java
+++ src/main/java/algorithm/BBAStarParallel/IBBAObservable.java
@@ -6,8 +6,28 @@ import visualisation.controller.IObserver;
 import java.util.Map;
 
 public interface IBBAObservable extends Runnable {
+    /**
+     * Used to add BBAStarParent as an observer to the child thread
+     * @param e is the BBAStarParent instance
+     */
     void addBBA(IBBAObserver e);
+
+    /**
+     * Used to remove BBAStarParent as an observer to the child thread
+     * @param e is the BBAStarParent instance
+     */
     void removeBBA(IBBAObserver e);
+
+    /**
+     * Method used by child thread to notify the parent BBAStarParent
+     * instance of any update to the child thread
+     */
     void notifyObserversOfSchedulingUpdateBBA();
+
+    /**
+     * Method used by child thread to notify the parent BBAStarParent
+     * instance that a solution has been found and the algorithm has
+     * ended
+     */
     void notifyObserversOfAlgorithmEndingBBA();
 }
